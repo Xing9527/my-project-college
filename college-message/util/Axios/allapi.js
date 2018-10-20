4e917690d13c575b16223a6feb021dc93d23d155
Xing9527 <929885647@qq.com>
2018-10-20 08:29:57 +0800

    供参考

---
+++ college-message/util/Axios/allapi.js
@@ -0,0 +1,47 @@
+export default {
+  qiniu:'qiniu/getToken',
+
+  //新闻列表
+  getNews: 'news/get',
+  addNews: 'news/add',
+  delNews: 'news/del',
+  updateNews: 'news/update',
+  addView: 'news/addView',
+
+
+  //招就列表
+  getJobs: 'jobs/get',
+  getList: 'jobs/list',
+  addJobs: 'jobs/add',
+  delJobs: 'jobs/del',
+  updateJobs: 'jobs/update',
+  viewJobs: 'jobs/addView',
+
+  //留言
+  getMsg: 'message/get',
+  addMsg: 'message/add',
+  delMsg: 'message/del',
+
+
+  //轮播图
+  getSwiper: 'swiper/get',
+  addSwiper: 'swiper/add',
+  delSwiper: 'swiper/del',
+  updateSwiper: 'swiper/update',
+
+  //管理员登录
+  adminLogin:'admin/login',
+  adminIsLogin:'admin/isLogin',
+  adminLogout:'admin/logout',
+  addAdmin:'admin/add',
+
+
+  //我想大声告诉你
+  sayGet:'say/get',
+  sayAdd:'say/add',
+  sayOpenOne:'say/openOne',
+  sayUpdate:'say/update',
+  sayOpen:'say/open',
+  sayClose:'say/close',
+  sayDel:'say/del'
+}
